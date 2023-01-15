76ef6b3e4f26958ff009e4dda1a295141daba698
seungm2n <seungm2n2@gmail.com>
2023-01-15 19:32:32 +0900

    사칙연산 모두 출력되는 코드

---
+++ CodingTest_BAEKJOON/src/kr/co/code/FourArithmeticOperations.java
@@ -0,0 +1,22 @@
+package kr.co.code;
+
+import java.util.Scanner;
+
+public class FourArithmeticOperations {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		
+		int a = s.nextInt();
+		int b = s.nextInt();
+		
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+		
+		s.close();
+	}
+
+}
