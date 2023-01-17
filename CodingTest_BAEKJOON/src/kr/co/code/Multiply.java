723adc3ed281b5035f668eb76d03b7383147d297
seungm2n <seungm2n2@gmail.com>
2023-01-17 14:22:35 +0900

    Multiply 추가

---
+++ CodingTest_BAEKJOON/src/kr/co/code/Multiply.java
@@ -0,0 +1,22 @@
+package kr.co.code;
+
+import java.util.Scanner;
+
+public class Multiply {
+ 
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+ 
+		int A = sc.nextInt();		// 첫번째 숫자는 int형으로 받음
+		String B = sc.next();		// 두번째 숫자는 String형으로 ㅂ다음
+        
+		sc.close();
+ 
+		// charAt()으로 하나씩 꺼내어 불러옴.
+		System.out.println(A * (B.charAt(2) - '0'));
+		System.out.println(A * (B.charAt(1) - '0'));
+		System.out.println(A * (B.charAt(0) - '0'));
+		System.out.println(A * Integer.parseInt(B));
+ 
+	}
+}
