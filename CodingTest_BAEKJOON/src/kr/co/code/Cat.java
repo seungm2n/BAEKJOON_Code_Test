6d02b7550cb8d3e1f8444b32388d9999b4891615
seungm2n <seungm2n2@gmail.com>
2023-01-19 18:40:39 +0900

    Cat Test

---
+++ CodingTest_BAEKJOON/src/kr/co/code/Cat.java
@@ -0,0 +1,15 @@
+package kr.co.code;
+
+public class Cat {
+    public static void main(String[] args){
+        
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
+//\    /\
+// )  ( ')
+//(  /  )
+//\(__)|
---
+++ CodingTest_BAEKJOON/src/kr/co/code/Cat2.java
@@ -0,0 +1,12 @@
+package kr.co.code;
+
+public class Cat2 {
+
+	public static void main(String[] args) {
+
+		System.out.println("\\    /\\\n" +
+							" )  ( ')\n" +
+							"(  /  )\n" +
+							" \\(__)|");
+	}
+}
