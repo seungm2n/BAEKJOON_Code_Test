4e4591e79c38fc72d813e59498a37ef8b04714e3
seungm2n <seungm2n2@gmail.com>
2023-01-20 13:45:19 +0900

    Dog Test

---
+++ CodingTest_BAEKJOON/src/kr/co/code/Dog2.java
@@ -0,0 +1,18 @@
+package kr.co.code;
+
+public class Dog2 {
+
+	public static void main(String[] args) {
+		
+		StringBuilder sb = new StringBuilder();
+			sb.append("|\\_/|\n");
+			sb.append("|q p|   /}\n");
+			sb.append("( 0 )\"\"\"\\\n");
+			sb.append("|\"^\"`    |\n");
+			sb.append("||_/=\\\\__|");
+			
+			System.out.println(sb);
+
+	}
+
+}
---
+++ CodingTest_BAEKJOON/src/kr/co/code/Dog3.java
@@ -0,0 +1,18 @@
+package kr.co.code;
+
+public class Dog3 {
+
+	public static void main(String[] args) {
+		
+		StringBuffer sb = new StringBuffer();
+			sb.append("|\\_/|\n");
+			sb.append("|q p|   /}\n");
+			sb.append("( 0 )\"\"\"\\\n");
+			sb.append("|\"^\"`    |\n");
+			sb.append("||_/=\\\\__|");
+			
+			System.out.println(sb);
+
+	}
+
+}
