19985353070a7c585831564b76f199d7bc7bd603
seungm2n <seungm2n2@gmail.com>
2023-01-20 19:52:09 +0900

    1단계 마지막 새싹 출력 완료

---
+++ CodingTest_BAEKJOON/src/kr/co/code/Sprout2.java
@@ -0,0 +1,15 @@
+package kr.co.code;
+
+public class Sprout2 {
+
+	public static void main(String[] args) {
+		System.out.println("         ,r'\"7\n"
+				+ "r`-_   ,'  ,/\n"
+				+ " \\. \". L_r'\n"
+				+ "   `~\\/\n"
+				+ "      |\n"
+				+ "      |");
+
+	}
+
+}
