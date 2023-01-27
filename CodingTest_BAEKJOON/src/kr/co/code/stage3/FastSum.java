9afe8fcca30f600c8dc593480352f2faa88e7cb5
seungm2n <seungm2n2@gmail.com>
2023-01-27 13:14:54 +0900

    ,

---
+++ CodingTest_BAEKJOON/src/kr/co/code/stage3/FastSum.java
@@ -0,0 +1,32 @@
+package kr.co.code.stage3;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class FastSum {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int t = Integer.parseInt(br.readLine());
+
+		StringTokenizer st;
+
+		for (int i = 0; i < t; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+
+			bw.write((Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken())) + "\n");
+
+		}
+		br.close();
+
+		bw.flush();
+		bw.close();
+	}
+
+}
