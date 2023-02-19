28723451b853e22b6a7d12d6c0fce54ff8adecfb
seungm2n <seungm2n2@gmail.com>
2023-02-19 18:34:40 +0900

    .

--- CodingTest_BAEKJOON/src/kr/co/code/stage5/NO1152BOJ2.java
+++ CodingTest_BAEKJOON/src/kr/co/code/stage5/NO1152BOJ2.java
@@ -10,10 +10,10 @@ public class NO1152BOJ2 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		// st 에 공백을 기준으로 나눈 토큰들을 st 에 저장한다
+		// 공백을 기준으로 나눈 토큰들을 저장한다
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-		// countTokens() 는 토큰의 개수를 반환한다
+		// countTokens()를 사용하여 토큰의 개수를 반환한다
 		System.out.println(st.countTokens());
 
 	}
